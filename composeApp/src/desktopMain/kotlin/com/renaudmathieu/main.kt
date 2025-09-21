c6086b3273c40d68b8b11e006a3ccabb208ccf60
Renaud Mathieu <reno.mathieu@gmail.com>
2025-09-21 12:18:32 +0200

    chore: set default window size for desktop ComposeApp

--- composeApp/src/desktopMain/kotlin/com/renaudmathieu/main.kt
+++ composeApp/src/desktopMain/kotlin/com/renaudmathieu/main.kt
@@ -1,12 +1,16 @@
 package com.renaudmathieu
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "GrooveBox",
+        state = rememberWindowState(size = DpSize(412.dp, 915.dp)),
     ) {
         App()
     }
